bdbcb1dd2861ffc12a72dfd4d0474179599b182c
rowanc1 <rowanc1@users.noreply.github.com>
2021-10-28 00:20:07 +0000

    deploy: 6bae9825cecc70abbb3ea23fdc7075946f55ea81

--- demo/index.js
+++ demo/index.js
@@ -16,18 +16,18 @@ const content = document.querySelector('#content');
 const options = {
     reducer: reducer_1.reducer,
     triggers: [
-        { name: 'hashtag', trigger: /(?:\s|\n|[^\d\w])(#)$/ },
+        // For demo purposes, make the `#` and `@` easier to create
+        { name: 'hashtag', trigger: /(#)$/ },
         { name: 'mention', trigger: /(@)$/ },
+        { name: 'emoji', trigger: ':' },
         { name: 'link', trigger: '[[', cancelOnFirstSpace: false },
         { name: 'jinja', trigger: '{{', cancelOnFirstSpace: false },
         { name: 'command', trigger: '/', decorationAttrs: { class: 'command' } },
-        { name: 'emoji', trigger: ':' },
         { name: 'variable', trigger: /((?:^[a-zA-Z0-9_]+)\s?=)$/, cancelOnFirstSpace: false },
         { name: 'code', trigger: /((?:[a-zA-Z0-9_]+)\.)$/ },
     ],
 };
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const view = new prosemirror_view_1.EditorView(editor, {
+window.view = new prosemirror_view_1.EditorView(editor, {
     state: prosemirror_state_1.EditorState.create({
         doc: prosemirror_model_1.DOMParser.fromSchema(prosemirror_schema_basic_1.schema).parse(content),
         plugins: [...(0, src_1.default)(options), ...(0, prosemirror_example_setup_1.exampleSetup)({ schema: prosemirror_schema_basic_1.schema, menuBar: false })],
