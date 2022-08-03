5493ee03fbd6bc79744f18113748e4dac76dd55b
Rowan Cockett <rowanc1@gmail.com>
2022-08-03 16:43:13 -0600

    🚧  Move to @curvenote/editor repository

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { autocomplete } from './plugin';
-
-export * from './types';
-export * from './utils';
-export * from './plugin';
-export * from './actions';
-
-export default autocomplete;
--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { PluginKey, Selection } from 'prosemirror-state';
-import { DecorationSet } from 'prosemirror-view';
-
-export const pluginKey = new PluginKey('autocomplete');
-
-export function inSuggestion(selection: Selection, decorations: DecorationSet) {
-  return decorations.find(selection.from, selection.to).length > 0;
-}
